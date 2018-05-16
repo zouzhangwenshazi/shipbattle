1bd2e591dc039a3ac158d5822088004123ddcdee
zouzhangwenshazi <37854085+zouzhangwenshazi@users.noreply.github.com>
2018-05-16 09:54:56 +0800

    Add files via upload

--- battleship.js
+++ battleship.js
@@ -1,31 +1,145 @@
-var randomLoc = Math.floor(Math.random() * 5);
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+	displayMiss: function(location){
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
+	}
+};
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
+view.displayMessage("Tap tap, is this thing on?");
 
-while (isSunk == false) {
-	guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-	if (guess < 0 || guess > 6) {
-		alert("Please enter a valid cell number!");
-	}	else {
-		guesses = guesses + 1;
+var ships = [ { locations: ["10", "20", "30"], hits: ["", "", ""] },
+							{ locations: ["32", "33", "34"], hits: ["", "", ""] },
+							{ locations: ["63", "64", "65"], hits: ["", "", "hit"] }]
 
-		if(guess == location1 || guess == location2 || guess == location3) {
-			alert("HIT!");
-			hits = hits + 1;
-			if (hits == 3) {
-				isSunk = true;
-				alert("You sank my battleship!");
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+
+	ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
+	{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+	{ locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+	fire: function(guess) {
+		for(var i = 0; i < this.numShips; i++) {
+				var ship = this.ships[i];
+				var index = ship.locations.indexOf(guess);
+				if (index >= 0) {
+					ship.hits[index] = "hit";
+					view.displayHit(guess);
+					view.displayMessage("HIT!");
+					if (this.isSunk(ship)) {
+						view.displayMessage("You sank my battleship!");
+						this.shipsSunk++;
+					}
+					return true;
+				}
+		}
+		view.displayMiss(guess);
+		view.displayMessage("You missed.");
+		return false;
+	},
+		isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
 			}
-		} else {
-			alert("MISS!");
+		}
+		return true;
+	}
+};
+
+function parseGuess(guess) {
+	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+	if(guess === null || guess.length !== 2) {
+		alert("Oops, please enter a letter and a number on the board.");
+	} else {
+		firstChar = guess.charAt(0);
+		var row = alphabet.indexOf(firstChar);
+		var column = guess.charAt(1);
+
+		if (isNaN(row) || isNaN(column)) {
+			alert("Oops, that isn't on the board.");
+		} else if (row < 0 || row >= model.boardSize ||
+				column < 0 || column >= model.boardSize) {
+					alert("Oops, that's off the board!");
+				} else {
+					return row + column;
+				}
+	}
+	return null;
+}
+
+// console.log(parseGuess("A0"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0"));
+// console.log(parseGuess("A7"));
+
+var controller = {
+	guesses: 0,
+
+	processGuess: function(guess) {
+		var location = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if(hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sank all my battleships, in " +
+			 this.guesses + " guesses");
+			}
 		}
 	}
+};
+
+// controller.processGuess("A0");
+
+// controller.processGuess("A6");
+// controller.processGuess("B6");
+// controller.processGuess("C6");
+
+// controller.processGuess("C4");
+// controller.processGuess("D4");
+// controller.processGuess("E4");
+
+// controller.processGuess("B0");
+// controller.processGuess("B1");
+// controller.processGuess("B2");
+
+function init() {
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handleKeyPress;
 }
-var states = "You took " + guesses + " guesses to sink the battleship, " + 
-	"which means your shooting accuracy was " + (3/guesses);
-alert(states);
+function handleFireButton() {
+	//从表单中获取值的代码
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value;
+	controller.processGuess(guess);
+
+	guessInput.value = "";
+}
+function handleKeyPress(e) {
+	var fireButton = document.getElementById("fireButton");
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false;
+	}
+}
+window.onload = init;
